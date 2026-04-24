dfaa33bfd9e59e917c2086d5e434c9762b3354a3
Aarav Kumar <aarav12f@gmail.com>
2026-04-24 09:23:53 +0530

    utils added

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,9 @@ import authRoutes from './routes/auth.route.js';
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
